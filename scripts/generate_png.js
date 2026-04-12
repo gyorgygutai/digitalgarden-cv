3dda723c537ec20842aa9138929134df61943272
Gyorgy Gutai <gyorgy.gutai@gmail.com>
2026-04-12 06:13:17 +0000

    Add css

--- scripts/generate_png.js
+++ scripts/generate_png.js
@@ -1,27 +1,28 @@
-import puppeteer from "puppeteer";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const htmlPath = path.resolve(__dirname, "../docs/index.html");
+const puppeteer = require("puppeteer");
+const path = require("path");
+const __dirname = __dirname || path.resolve();
 
-const browser = await puppeteer.launch({
-  headless: true,
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
-  args: ["--no-sandbox", "--disable-setuid-sandbox"],
-});
-const page = await browser.newPage();
+(async () => {
+  const htmlPath = path.resolve(__dirname, "../docs/index.html");
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
 
-await page.goto(`file://${htmlPath}`, {
-  waitUntil: "networkidle2",
-});
+  await page.goto(`file://${htmlPath}`, {
+    waitUntil: "networkidle2",
+  });
 
-await page.addStyleTag({
-  path: path.resolve(__dirname, "../docs/assets/print.css"),
-});
+  await page.addStyleTag({
+    path: path.resolve(__dirname, "../docs/assets/print.css"),
+  });
 
-await page.screenshot({
-  path: path.resolve(__dirname, "../docs/cv_gyorgy_gutai.png"),
-  fullPage: true,
-});
-await browser.close();
+  await page.screenshot({
+    path: path.resolve(__dirname, "../docs/cv_gyorgy_gutai.png"),
+    fullPage: true,
+  });
+  await browser.close();
+})();
